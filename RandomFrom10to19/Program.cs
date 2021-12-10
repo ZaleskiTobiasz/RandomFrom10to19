bffa5071c4a32a3848278a6f5443304e09885645
ZaleskiTobiasz <tobiasz.zaleski@capgemini.com>
2021-12-10 14:53:57 +0100

    Add project files.

---
+++ RandomFrom10to19/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace RandomFrom10to19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RNGenerator_AndChecker rng_AndChecker = new RNGenerator_AndChecker();
+            if (rng_AndChecker.isItDoneAfterFstIteration())
+            {
+
+            }
+            else
+            {
+                rng_AndChecker.ContinueWork();
+            }
+        }
+    }
+}
---
+++ RandomFrom10to19/RNGenerator_AndChecker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomFrom10to19
+{
+    public class RNGenerator_AndChecker
+    {
+        List<byte> numbers = new List<byte>();
+        public Random rng = new Random();
+        int index = 0;
+        bool isDone = false;
+        public RNGenerator_AndChecker()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                numbers.Add((byte)rng.Next(10, 20));
+                Console.WriteLine(numbers[i]);
+                index = i;
+            }
+            if (numbers[0] % 3 == 0 && numbers[1] % 3 == 0 && numbers[2] % 3 == 0)
+            {
+                Console.WriteLine("Ostatnie 3 liczby podzielne przez trzy, ilość wylosowanych liczb = " + (index + 1));
+                isDone = true;
+            }
+        }
+
+        public void ContinueWork()
+        {
+            while (true)
+            {
+                if (numbers[index - 2] % 3 == 0 && numbers[index - 1] % 3 == 0 && numbers[index] % 3 == 0)
+                {
+                    Console.WriteLine("Ostatnie 3 liczby podzielne przez trzy, ilość wylosowanych liczb = " + (index + 1));
+                    break;
+                }
+                else
+                {
+                    GenerateAnotherNumber();
+                }
+            }
+
+        }
+        public bool isItDoneAfterFstIteration()
+        {
+            return isDone;
+        }
+        public void GenerateAnotherNumber()
+        {
+            numbers.Add((byte)rng.Next(10, 20));
+            index++;
+            Console.WriteLine(numbers[index]);
+        }
+
+    }
+}
